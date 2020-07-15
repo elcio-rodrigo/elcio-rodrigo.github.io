4bb72454d30d73103891c9ec5fe5dfcf19346c83
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-15 13:46:58 -0300

    resolução exercício 3

--- exercises/4_3/exercicio2.js
+++ exercises/4_3/exercicio2.js
@@ -1,8 +1,8 @@
 let n = 5;
-let asterisco = " * ";
-let linha = "";
+let asterisco = '*';
+let linha = '';
 
-for (let i = 0; i < n; i++) {
-    console.log(linha);
-    linha += asterisco;
-}
+for (let i = 0; i <= n; i++) {
+  console.log(linha);
+  linha += asterisco;
+};
